96aa2be2c41c87e51bea5a34e0f855dbf092fe27
taik792 <rosario79@libero.it>
2026-09-05 17:44:45 +0200

    Update trova_coppia_perfetta.py

--- trova_coppia_perfetta.py
+++ trova_coppia_perfetta.py
@@ -16,7 +16,7 @@ def analizza_tutte_le_coppie():
         return
 
     # Ruota di partenza impostata su MILANO
-    RUOTA_PARTENZA = "TORINO 
+    RUOTA_PARTENZA = "TORINO"
 
     with open('estrazioni.json', 'r', encoding='utf-8') as f:
         archivio = json.load(f)
